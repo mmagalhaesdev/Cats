47f39ad525401eaaf8e842015d0ba74a6ee04024
Murilo <mmagalhaesromualdo@gmail.com>
2025-08-16 11:02:22 -0300

    Adicionando design de post

--- Controller/processa_login.php
+++ Controller/processa_login.php
@@ -1,25 +1,25 @@
 <?php
-session_start();
-include '../Model/db.php';
-$email = $_POST['email'];
-$senha = $_POST['senha'];
-$sql = "SELECT * FROM usuario WHERE email = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("s", $email);
-$stmt->execute();
-$result = $stmt->get_result();
-if ($usuario = $result->fetch_assoc()) {
-if (password_verify($senha, $usuario['senha'])) {
-// Cria a sessão com o ID do usuário
-$_SESSION['usuario_id'] = $usuario['id'];
-// Redireciona para o feed
-header("Location: ../View/novo_post.php");
-exit;
-} else {
-echo "Senha incorreta.";
-}
-} else {
-echo "Usuário não encontrado.";
-}
-$conn->close();
-?>
+ session_start();
+ include '../Model/db.php';
+ $email = $_POST['email'];
+ $senha = $_POST['senha'];
+ $sql = "SELECT * FROM usuario WHERE email = ?";
+ $stmt = $conn->prepare($sql);
+ $stmt->bind_param("s", $email);
+ $stmt->execute();
+ $result = $stmt->get_result();
+ if ($usuario = $result->fetch_assoc()) {
+    if (password_verify($senha, $usuario['senha'])) {
+        // Cria a sessão com o ID do usuário
+        $_SESSION['usuario_id'] = $usuario['id'];
+        // Redireciona para o feed
+        header("Location: ../View/novo_post.php");
+        exit;
+    } else {
+        echo "Senha incorreta.";
+    }
+ } else {
+    echo "Usuário não encontrado.";
+ }
+ $conn->close();
+ ?>
--- View/novo_post.php
+++ View/novo_post.php
@@ -13,28 +13,27 @@ exit;
 <title>Novo Post</title>
 <link rel="stylesheet" href="../css/style.css">
 <style>
-
+    .form1 form{
+        border: 2px solid #E3E3E3;
+        border-radius: 12px;
+    }
+    p{
+        padding-left: 35px;
+        font-size: 20px; 
+        font-family: Roboto Condensed;
+        color: #BFBFBF;
+    }
     .form1 input{
         font-size: 15px; 
         font-family: Roboto Condensed;
         border-radius: 6px;
-        padding-top: 10px;
     }
     .form1 h2{
+        padding-left: 35px;
+        padding-top: 35px;
         font-size: 30px; 
         font-family: Roboto Condensed;
         color: #FF6554;
-    }
-    .form1 label{
-        font-size: 15px; 
-        font-family: Roboto Condensed;
-        color: #FF6554;
-    }
-    .form1 p, a{
-        font-family: Roboto Condensed;
-        font-size: 15px; 
-        text-decoration: none;
-        color: black;
     }
     .form1 button{
         width: 120px;
@@ -44,7 +43,36 @@ exit;
         border: 1px solid #FF6554;
         border-radius: 6px;
     }
-
+    #imagem{
+        display: none;
+    }
+    .imagem2{
+        background-color: #FFA255;
+        padding: 7px;
+        font-size: 18px; 
+        font-family: Roboto Condensed;
+        color: #FFFFFF;
+        border-radius: 6px;
+    }
+    .file{
+        padding-left: 35px;
+    }
+    .legenda{
+         padding-left: 35px;
+         padding-right: 35px;
+    }
+    .postar{
+        padding: 0px 0px 20px 35px;
+    }
+    #legend{
+        border: 3px solid #FF6554;
+        border-radius: 6px;
+        height: 118px;
+        font-family: Roboto Condensed;
+        color: #BFBFBF;
+        padding: 10px;
+        font-size: 15px;
+    }
 
 </style>
 </head>
@@ -67,12 +95,19 @@ exit;
         <form action="salvar_post.php" method="POST" enctype="multipart/form-data">
             <h2>Criar novo post</h2>
             <br>
-            <label>Imagem:</label>
+            <p>Imagem:</p>
+            <input type="file" id="imagem" name="imagem" accept="image/*" required><br>
+            <div class="file">
+            <label for="imagem" class="imagem2">Escolher Arquivo</label>
+            </div>
             <br>
-            <input type="file" name="imagem" accept="image/*" required><br><br>
-            <br>
-            <textarea name="legenda" placeholder="Legenda" rows="4" cols="40" required></textarea><br><br>
+            <div class="legenda">
+            <textarea name="legenda" id ="legend"placeholder="Legenda" rows="4" cols="40" required></textarea>
+            </div>
+            <br><br>
+            <div class="postar">
             <button type="submit">Postar</button>
+            </div>
         </form>
     </div>
 </div>
